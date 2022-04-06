8a8a537a87ebecd463a426c711f5d641e64f8fa5
Karthik <jeefkarthik036@gmail.com>
2022-04-06 20:14:14 +0530

    added-UC4

--- LinkedList/LinkedListMain.java
+++ LinkedList/LinkedListMain.java
@@ -2,18 +2,25 @@ package com.bridge.LinkedList;
 
 import java.util.Scanner;
 
+/**
+ * Hello world!
+ */
 public class LinkedListMain {
     public static void main(String[] args) {
         System.out.println("Welcome to linkedList DataStructure Program");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 1 to add the data at the start");
         System.out.println("Enter 2 to add the data at the end");
+        System.out.println("Enter 3 to insert data inBetween");
         switch (scanner.nextInt()) {
             case 1:
                 Operations.addDataAtStart();
                 break;
             case 2:
                 Operations.addDataAtEnd();
+                break;
+            case 3:
+                Operations.insertInBetween();
                 break;
         }
 
