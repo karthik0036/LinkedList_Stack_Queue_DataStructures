d3c979aa2a0276225dd9c46949e3db03b9f8b56a
Karthik <jeefkarthik036@gmail.com>
2022-04-06 19:23:50 +0530

    added-UC1

---
+++ LinkedList/LinkedListMain.java
@@ -0,0 +1,21 @@
+package com.bridge.LinkedList;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to LinkedList Datastructure Program");
+        Node firstNode = new Node(56);
+        Node secondNode = new Node(30);
+        Node thirdNode = new Node(70);
+        Node head = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        Node tail = thirdNode;
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("Null");
+    }
+}
